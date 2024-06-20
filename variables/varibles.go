120216e8537ac615371ade71ea49e067ec86bbb5
lundjrl <jrlbidamin@gmail.com>
2024-06-20 07:43:37 -0400

    init

---
+++ variables/varibles.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+  var a = "initial"
+  fmt.Println(a)
+
+  var b,c int = 1,2
+  var d,e,f = false,1,"yes"
+
+  var g,h,i,j = 2,"test",false,3.14
+  var k float64 = 6.66
+
+  fmt.Println(b,c,d,e,f,g,h,i,j,k)
+
+  
+  l := "apple"
+  fmt.Println(l)
+
+  var zero int
+  fmt.Println(zero)
+
+
+  var zerobool bool
+  var zerostr string
+
+  fmt.Println(zerobool, zerostr)
+}
